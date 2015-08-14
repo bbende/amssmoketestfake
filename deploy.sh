4a549ad3b9ec05a71fb0300eb8d77310a7ee222a
Bryan Bende <bbende@apache.org>
2015-08-14 15:37:28 -0400

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+scp -r -P 2222 ./common-services/* root@127.0.0.1:/var/lib/ambari-server/resources/common-services/
+scp -r -P 2222 ./stacks/HDP/2.3/services/* root@127.0.0.1:/var/lib/ambari-server/resources/stacks/HDP/2.3/services/ 
